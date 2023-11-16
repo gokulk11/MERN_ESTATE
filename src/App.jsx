1de08b627455e3590a08c2756e0e36e894815357
gokulk11 <gokulk11.gb@gmail.com>
2023-11-16 16:19:49 +0530

    Creating Header Components and enable Routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
+import React from "react";
+import Header from "./components/Header";
+import { Outlet } from "react-router-dom";
 
 const App = () => {
   console.log("Hello world");
   return (
-    <h1 className=''>App</h1>
-  )
-}
+    <>
+      <Header/>
+      <Outlet />
+    </>
+  );
+};
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -6,11 +6,30 @@ import {
 } from "react-router-dom";
 import App from './App.jsx'
 import './index.css'
+import ErrorPage from './Pages/ErrorPage.jsx';
+import Home from './Pages/Home.jsx';
+import About from './Pages/About.jsx';
+import SignIn from './Pages/SignIn.jsx';
 
 const router = createBrowserRouter([
   {
     path: "/",
     element:<App />,
+    errorElement:<ErrorPage/>,
+    children:[
+      {
+        path: "/",
+        element:<Home/>
+      },
+      {
+        path: "/about",
+        element:<About/>
+      },
+      {
+        path: "/signin",
+        element:<SignIn/>
+      }
+    ] 
   },
 ]);
 
