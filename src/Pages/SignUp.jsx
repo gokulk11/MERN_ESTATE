43e5ab26b64a0e229486ccaf028beef4dd982daf
gokulk11 <gokulk11.gb@gmail.com>
2023-11-26 18:54:12 +0530

    signup page connection done

--- src/Pages/SignUp.jsx
+++ src/Pages/SignUp.jsx
@@ -1,35 +1,45 @@
 import axios from "axios";
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link,useNavigate } from "react-router-dom";
 
 const SignUp = () => {
-const [formData, setFormData] = useState({})
-function handleChange(e){
-    setFormData(
-      {
-        ...formData,
-        [e.target.id]:e.target.value
-      }
-    )
-}
-
-const handleSubmit = async (e) => {
-  e.preventDefault();
-
-  try {
-      const res = await axios.post('/api/auth/signup', formData, {
-          headers: {
-              'Content-Type': 'application/json',
-          },
-      });
-
-      console.log(res.data);
-      // You can handle the response data as needed
-  } catch (error) {
-      console.error('Error:', error);
-      // Handle errors here
+  const [formData, setFormData] = useState({});
+  const [error, setError] = useState(null);
+  const navigate = useNavigate()
+  const [loading, setLoading] = useState(false);
+  function handleChange(e) {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
   }
-};
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      const res = await fetch("/api/auth/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      console.log(data);
+      if (data.success === false) {
+        setLoading(false);
+        setError(data.message);
+        return;
+      }
+      setLoading(false);
+      setError(null);
+      navigate('/signin');
+    } catch (error) {
+      setLoading(false);
+      setError(error.message);
+    }
+  };
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl text-center font-semibold my-7">Sign Up</h1>
@@ -55,16 +65,19 @@ const handleSubmit = async (e) => {
           id="password"
           onChange={handleChange}
         />
-        <button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">
-          SignUp
+        <button
+          disabled={loading}
+          className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">
+          {loading ? "Loading..." : "Sign Up"}
         </button>
       </form>
       <div className="flex gap-2 mt-5 justify-center">
         <p>Have an account?</p>
         <Link to="/signin">
-        <span className="text-blue-700">Sign in</span>
+          <span className="text-blue-700">Sign in</span>
         </Link>
       </div>
+        {error && <p className="text-red-500 mt-5">{error}</p>}
     </div>
   );
 };
