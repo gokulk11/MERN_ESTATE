aab2a17187efc4b31cc1669a47e26cd4eaab297e
gokulk11 <gokulk11.gb@gmail.com>
2023-11-26 15:46:26 +0530

    signup the new user

--- src/Pages/SignUp.jsx
+++ src/Pages/SignUp.jsx
@@ -1,28 +1,59 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 const SignUp = () => {
+const [formData, setFormData] = useState({})
+function handleChange(e){
+    setFormData(
+      {
+        ...formData,
+        [e.target.id]:e.target.value
+      }
+    )
+}
+
+const handleSubmit = async (e) => {
+  e.preventDefault();
+
+  try {
+      const res = await axios.post('/api/auth/signup', formData, {
+          headers: {
+              'Content-Type': 'application/json',
+          },
+      });
+
+      console.log(res.data);
+      // You can handle the response data as needed
+  } catch (error) {
+      console.error('Error:', error);
+      // Handle errors here
+  }
+};
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl text-center font-semibold my-7">Sign Up</h1>
-      <form action="" className="flex flex-col gap-4">
+      <form onSubmit={handleSubmit} action="" className="flex flex-col gap-4">
         <input
           type="text"
           placeholder="username"
           className="border p-3 rounded-lg"
           id="username"
+          onChange={handleChange}
         />
         <input
-          type="text"
+          type="email"
           placeholder="email"
           className="border p-3 rounded-lg"
           id="email"
+          onChange={handleChange}
         />
         <input
-          type="text"
+          type="password"
           placeholder="password"
           className="border p-3 rounded-lg"
           id="password"
+          onChange={handleChange}
         />
         <button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">
           SignUp
