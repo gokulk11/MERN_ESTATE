e463f2f56585ced576b11738fa003c140776b175
gokulk11 <gokulk11.gb@gmail.com>
2023-11-27 15:52:33 +0530

    Setup Private Route

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,27 @@
-import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
 import Header from "./components/Header";
-import { Outlet } from "react-router-dom";
+import PrivateRoute from "./components/PrivateRoute";
 
 const App = () => {
-  console.log("Hello world");
   return (
     <>
-      <Header/>
-      <Outlet />
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/about" element={<About />} />
+          <Route element={<PrivateRoute />}>
+            <Route path="/profile" element={<Profile />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -1,47 +1,15 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import App from "./App.jsx";
-import "./index.css";
-import ErrorPage from "./Pages/ErrorPage.jsx";
-import Home from "./Pages/Home.jsx";
-import {store, persistor} from "./redux/store.js";
-import { Provider } from "react-redux";
-import About from "./Pages/About.jsx";
-import SignIn from "./Pages/SignIn.jsx";
-import SignUp from "./Pages/SignUp.jsx";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import { persistor, store } from './redux/store.js';
+import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    errorElement: <ErrorPage />,
-    children: [
-      {
-        path: "/",
-        element: <Home />,
-      },
-      {
-        path: "/about",
-        element: <About />,
-      },
-      {
-        path: "/signin",
-        element: <SignIn />,
-      },
-      {
-        path: "/signup",
-        element: <SignUp />,
-      },
-    ],
-  },
-]);
-
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
-      <RouterProvider router={router} />
+      <App />
     </PersistGate>
   </Provider>
 );
